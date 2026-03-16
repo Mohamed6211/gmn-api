fb734783a5fd6f109841ed1925d310774b3a7bf1
Mohamed6211 <88840019+Mohamed6211@users.noreply.github.com>
2026-03-16 13:50:12 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-pip install "pip<25"
-pip install "setuptools<70"
-pip install wheel
+#!/bin/bash
+# Create virtual environment
+python -m venv .venv
+source .venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
