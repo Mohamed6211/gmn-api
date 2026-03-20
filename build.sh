fa56c2fcfd14dac030edb3219d842fcee5567cbe
Mohamed6211 <88840019+Mohamed6211@users.noreply.github.com>
2026-03-20 12:11:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Upgrade pip first
-python3 -m pip install --upgrade pip
-
-# Install dependencies from requirements.txt
-python3 -m pip install --no-cache-dir -r requirements.txt
+pip install --upgrade pip
+pip install --no-cache-dir -r requirements.txt
